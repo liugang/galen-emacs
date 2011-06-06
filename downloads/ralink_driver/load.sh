80806c49daec5fe8441234f808ca36d5cd239413
Galen <kuaiyue2311@gmail.com>
2011-06-06 23:08:56 +0800

    commit load.sh and unload.sh

---
+++ downloads/ralink_driver/load.sh
@@ -0,0 +1,5 @@
+insmod rtutil5370sta.ko
+insmod rt5370sta.ko
+insmod rtnet5370sta.ko
+ifconfig ra0 up
+ifconfig p2p0 up
---
+++ downloads/ralink_driver/unload.sh
@@ -0,0 +1,4 @@
+ifconfig p2p0 down
+ifconfig ra0 down
+rmmod rtnet5370sta rt5370sta rtutil5370sta
+
